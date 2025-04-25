3350ec06e175293120f7eca0f8286922ac2811cd
Patrick <patrickbpds@gmail.com>
2025-04-25 10:36:57 -0300

    feat: added logo in banner component

--- src/components/ad-banner.tsx
+++ src/components/ad-banner.tsx
@@ -12,7 +12,15 @@ export function AdBanner() {
     <Card className="h-[200px] overflow-hidden">
       <Link href="/" className="block h-full">
         <CardContent className="p-0 h-full flex flex-row items-center">
-          <div></div>
+          <div className="relative h-full w-1/2">
+            <image
+              src="/logo-dashboard.png"
+              alt="Mobile App Logo"
+              width={200}
+              height={200}
+              objectFit="cover"
+            />
+          </div>
           <div className="p-4 w-1/2">
             <CardTitle>Download Our App!</CardTitle>
             <CardDescription>
