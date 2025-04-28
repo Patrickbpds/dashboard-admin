7d542b88f44096af900111553603f1c111dfcaa5
Patrick <patrickbpds@gmail.com>
2025-04-28 15:16:47 -0300

    fix: layout aligned with the sidebarInset component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { SidebarProvider, SidebarInset } from "@/components/ui/sidebar";
 
 import { Header } from "@/components/header";
 
@@ -32,9 +32,10 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <SidebarProvider>
-          <Header />
-          <SidebarTrigger />
-          {children}
+          <SidebarInset>
+            <Header />
+            {children}
+          </SidebarInset>
         </SidebarProvider>
       </body>
     </html>
